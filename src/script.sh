a80f159a09d08ec0ab8cbe95cc7b325063ed9359
Jedrzej Zawojski <jedrzej.zawojski95@gmail.com>
2026-04-15 14:18:09 +0200

    feat: update for save_brew_install

--- src/script.sh
+++ src/script.sh
@@ -3,6 +3,14 @@
 set -e
 set -o pipefail
 
+brew_install() {
+    brew list --formula "$1" &>/dev/null && printf "\e[93m%s\e[m\n" "$1 already installed, skipping." || brew install "$1"
+}
+
+brew_install_cask() {
+    brew list --cask "$1" &>/dev/null && printf "\e[93m%s\e[m\n" "$1 already installed, skipping." || brew install --cask "$1"
+}
+
 # Install brew
 if ! hash brew 2>/dev/null; then
     printf ">\tInstalling brew\n"
@@ -16,96 +24,104 @@ fi
 
 # Install CURL / WGET
 printf ">\tInstalling CURL/WGET\n"
-brew install curl
-brew install wget
+brew_install curl
+brew_install wget
 
 # Install GIT
 printf ">\tInstalling git\n"
-brew install git
+brew_install git
 
 # Install ZSH
 printf ">\tInstalling ZSH\n"
-brew install zsh zsh-completions
+brew_install zsh
+brew_install zsh-completions
 
 # Install Oh My ZSH
 printf ">\tInstalling Oh My ZSH\n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+[ -d "$HOME/.oh-my-zsh" ] && printf "\e[93m%s\e[m\n" "Oh My ZSH already installed, skipping." || \
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install ZSH plugins as Oh My ZSH custom plugins
 printf ">\tInstalling ZSH plugins\n"
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && \
+    printf "\e[93m%s\e[m\n" "zsh-autosuggestions already installed, skipping." || \
+    git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+[ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] && \
+    printf "\e[93m%s\e[m\n" "zsh-syntax-highlighting already installed, skipping." || \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 # Install oh-my-posh
 printf ">\tInstalling oh-my-posh\n"
-brew install oh-my-posh
+brew_install oh-my-posh
 
 # Install iTerm2
 printf ">\tInstalling terminal tools\n"
-brew install --cask iterm2
-brew install --cask ghostty
+brew_install_cask iterm2
+brew_install_cask ghostty
 
 # Install command line tools
 printf ">\tInstalling CLI tools\n"
-brew install vim
-brew install lsd
-brew install tree
-brew install jq
-brew install fzf
-brew install ripgrep
-brew install gh
+brew_install vim
+brew_install lsd
+brew_install tree
+brew_install jq
+brew_install fzf
+brew_install ripgrep
+brew_install gh
 
 # Browsers
 printf ">\tInstalling browsers\n"
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask arc
+brew_install_cask google-chrome
+brew_install_cask firefox
+brew_install_cask arc
 
 # Spotify
-brew install --cask spotify
+brew_install_cask spotify
 
 # Productivity
 printf ">\tInstalling Productivity Apps\n"
-brew install --cask notion
-brew install --cask notion-calendar
-brew install --cask notion-mail
-brew install --cask slack
-brew install --cask rectangle
-brew install --cask postman
-brew install --cask raycast
+brew_install_cask notion
+brew_install_cask notion-calendar
+brew_install_cask notion-mail
+brew_install_cask slack
+brew_install_cask rectangle
+brew_install_cask postman
+brew_install_cask raycast
 
 # IDEs
 printf ">\tInstalling IDEs\n"
-brew install --cask visual-studio-code
-brew install --cask cursor
-brew install --cask zed
-brew install --cask gitbutler
+brew_install_cask visual-studio-code
+brew_install_cask cursor
+brew_install_cask zed
+brew_install_cask gitbutler
 
 # Apps
 printf ">\tInstalling Apps\n"
-brew install --cask claude
-brew install --cask discord
-brew install --cask 1password
-brew install 1password-cli
+brew_install_cask claude
+brew_install_cask discord
+brew_install_cask 1password
+brew_install 1password-cli
 
 # Languages
 printf ">\tInstalling languages\n"
-brew install nvm
+brew_install nvm
 mkdir -p "$HOME/.nvm"
-brew install pnpm
-brew install bun
+brew_install pnpm
+
+brew tap oven-sh/bun
+brew_install bun
 
 # Install Docker
 printf ">\tInstalling Docker\n"
-brew install --cask docker
+brew_install_cask docker
 
 # Install Logitech Options
 printf ">\tInstalling Logitech Options\n"
-brew install --cask logi-options-plus
+brew_install_cask logi-options-plus
 
 # Fonts
 printf ">\tInstalling fonts\n"
-brew install --cask font-jetbrains-mono-nerd-font
+brew_install_cask font-jetbrains-mono-nerd-font
 
 # macOS defaults
 printf ">\tConfiguring macOS defaults\n"
